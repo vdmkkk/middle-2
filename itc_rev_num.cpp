70798ea78ffa06877a74928911fd26a38ae3b24c
Vadim Soloviov <50838513+Vodemka13@users.noreply.github.com>
2021-10-15 09:53:30 +0300

    Update itc_rev_num.cpp

--- itc_rev_num.cpp
+++ itc_rev_num.cpp
@@ -2,7 +2,7 @@
 
 int itc_rev_num(long long number){
     if (number < 0) number = -number;
-    if(number == 0) return 0;
+    if(number == 0) return 1;
     while(number % 10 == 0){
         number /= 10;
     }
