e8522e96dd8d05c9213df06dc14a696ae8048d4d
Vadim Soloviov <50838513+Vodemka13@users.noreply.github.com>
2021-10-15 09:26:38 +0300

    Update itc_mirror_count.cpp

--- itc_mirror_count.cpp
+++ itc_mirror_count.cpp
@@ -1,6 +1,7 @@
 #include "middle.h"
 
 int itc_mirror_count(long long number){
+    if (number < 0) number *= -1;
     int counter = 0;
     for (int i=0; i < number; i++){
         if (itc_min_num(i)) counter += 1;
