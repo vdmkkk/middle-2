2b6f381a4a36cf5d967c9d48715c5c9b51efe047
Vadim Soloviov <50838513+Vodemka13@users.noreply.github.com>
2021-10-15 10:28:55 +0300

    Update itc_mirror_count.cpp

--- itc_mirror_count.cpp
+++ itc_mirror_count.cpp
@@ -2,7 +2,7 @@
 
 int itc_mirror_count(long long number){
     int counter = 0;
-    for (int i=1; i < number; i++){
+    for (int i=1; i <= number; i++){
         if (itc_mirror_num(i)) counter += 1;
     }
     return counter;
