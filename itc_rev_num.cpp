312d04df4889391dae0417d18351ed7175d38918
Vadim Soloviov <50838513+Vodemka13@users.noreply.github.com>
2021-10-15 09:40:46 +0300

    Update itc_rev_num.cpp

--- itc_rev_num.cpp
+++ itc_rev_num.cpp
@@ -2,27 +2,9 @@
 
 int itc_rev_num(long long number){
     if (number < 0) number = -number;
-    int counter = 0;
-    bool firstTime = true;
-    bool flag = false;
-    while (number > 0){
-        if (firstTime) {
-            if (number % 10 == 0) flag = false;
-            else{
-                flag = true;
-                firstTime = false;
-            }
-        }
-        if (flag) {
-            counter += 1;
-            number /= 10;
-        }
-        else {
-            if (number % 10 != 0){
-                counter += 1;
-            }
-            number /= 10;
-        }
+    if(number == 0) return 0;
+    while(number % 10 == 0){
+        number /= 10;
     }
-    return counter;
+    return itc_len_num(number);
 }
