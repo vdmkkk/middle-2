8486d5f6b0fa7c97272b2c83a8df91474c336461
Vadim Soloviov <50838513+Vodemka13@users.noreply.github.com>
2021-10-17 18:12:48 +0300

    Update itc_second_simple_max_num.cpp

--- itc_second_simple_max_num.cpp
+++ itc_second_simple_max_num.cpp
@@ -1,16 +1,13 @@
 #include "middle.h"
 
 int itc_second_simple_max_num(long long number){
-    if (number < 0) number = -number;
-    int maxx = 0;
-    int maxx2 = 0;
-    while (number > 0){
-        if (number % 10 >= maxx){
-            maxx2 = maxx;
-            maxx = number % 10;
-        }
+    if(number < 0) number *= -1;
+    int res = 0;
+    int num2 = number;
+    if(itc_min_num(number) == itc_max_num(number)) return -1;
+    while(number > 0){
+        if(number % 10 > res && number % 10 < itc_max_num(num2)) res = number % 10;
         number /= 10;
     }
-    if (maxx2 == maxx) return -1;
-    return maxx2;
+    return res;
 }
