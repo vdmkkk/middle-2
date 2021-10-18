56c0875e9d8dc2199a66b1bdf5d7ea1cc30c2091
Vadim Soloviov <50838513+Vodemka13@users.noreply.github.com>
2021-10-18 11:38:20 +0300

    Update itc_bin_num.cpp

--- itc_bin_num.cpp
+++ itc_bin_num.cpp
@@ -5,9 +5,9 @@ long long itc_bin_num(long long number) {
     long long res = 0;
     int counter = 0;
     while (num > 0){
-        res += (num % 2) * itc_pow(10, counter);
+        res += (number % 2) * itc_pow(10, counter);
         counter += 1;
-        num /= 2;
+        number /= 2;
     }
     return res;
 }
